0cef095b40062ab26bbabe3c309671828b85a64f
Victor <victtavares1@gmail.com>
2015-03-20 14:04:04 -0300

    Fixing bugs

--- ScrollableViewController.swift
+++ ScrollableViewController.swift
@@ -1,105 +1,118 @@
 //
 //  ScrollableViewController.swift
-//  DismissKeyboard
+//  OpinionsApp
 //
-//  Created by Victor Tavares on 3/12/15.
-//  Copyright (c) 2015 Private. All rights reserved.
+//  Created by eCGlobal App 02 on 13/03/15.
+//  Copyright (c) 2015 eCGlobalSolutions. All rights reserved.
 //
 
 import UIKit
 
 class ScrollableViewController: UIViewController {
+    
+    var activeTextField:UITextField!
+    var scrollAmount:CGFloat = 0.0
+    var textFieldOffSet:CGFloat = 60
+    private var initialFrame: CGRect!
+    
+    override func viewDidLoad() {
+        initialFrame = self.view.frame
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        registerForKeyBoardNotifications()
+        
 
-        var activeTextField:UITextField!
-        var scrollAmount:CGFloat = 0.0
-        var moveViewUp = false
-        var textFieldOffSet:CGFloat = 60
+    }
     
-    
-        override func viewWillAppear(animated: Bool) {
-            super.viewWillAppear(animated)
-            registerForKeyBoardNotifications()
+    override func viewWillDisappear(animated: Bool) {
+        super.viewDidAppear(animated)
+        //Removing the selector when the user go to another screen
+        for myView in self.view.subviews {
+            if myView.isFirstResponder() && myView.isKindOfClass(UITextField){
+                myView.resignFirstResponder()
+                //println("Resign keyboard")
+            }
         }
-    
-        override func viewDidDisappear(animated: Bool) {
-            super.viewWillDisappear(animated)
-            NSNotificationCenter.defaultCenter().removeObserver(self)
-        }
-    
-        //Register Keyboard Notifications
-        func registerForKeyBoardNotifications() {
-            let notificationCenter = NSNotificationCenter.defaultCenter()
-            notificationCenter.addObserver(self, selector: "keyboardWasShow:", name: UIKeyboardDidShowNotification, object: nil)
-            notificationCenter.addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
-    
-        }
+        
+    }
     
     
-        func keyboardWasShow(aNotification:NSNotification) {
-            //println("Keyboard will show up")
-            let info:NSDictionary = aNotification.userInfo!
-            let value: NSValue = info.valueForKey(UIKeyboardFrameBeginUserInfoKey) as NSValue
-            let keyboardSize: CGSize = value.CGRectValue().size
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
     
     
-            var bottom = activeTextField.frame.origin.y + activeTextField.frame.height + textFieldOffSet
+    //Register Keyboard Notifications
+    func registerForKeyBoardNotifications() {
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "keyboardWasShow:", name: UIKeyboardDidShowNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+        
+    }
+    
+    
+    func keyboardWasShow(aNotification:NSNotification) {
+        //println("Keyboard will show up")
+        let info:NSDictionary = aNotification.userInfo!
+        let value: NSValue = info.valueForKey(UIKeyboardFrameBeginUserInfoKey) as NSValue
+        let keyboardSize: CGSize = value.CGRectValue().size
+        
+
+        if (activeTextField != nil) {
+            //var bottom = activeTextField.frame.origin.y + activeTextField.frame.height + textFieldOffSet + view.frame.origin.y
+            let superViewFrame = activeTextField.superview?.frame
+            var bottom = activeTextField.frame.origin.y + activeTextField.frame.height + textFieldOffSet + superViewFrame!.origin.y
             scrollAmount = keyboardSize.height - (view.frame.height - bottom)
-    
-    
+            //println(scrollAmount)
             if scrollAmount > 0 {
-                moveViewUp = true
                 scrollView(true)
-            } else {
-                moveViewUp = false
             }
         }
+    }
     
     
-        func keyboardWillBeHidden(aNotification: NSNotification) {
-            //println("Keyboard will be hidden")
-            if moveViewUp {
-                scrollView(false)
-            }
-        }
+    func keyboardWillBeHidden(aNotification: NSNotification) {
+        //println("Keyboard will be hidden")
+        scrollView(false)
+        
+    }
     
-        func scrollView(move:Bool) {
-            println("Before:\(scrollAmount)")
-            UIView.beginAnimations(nil, context: nil)
-            UIView.setAnimationDuration(0.3)
-    
-            if move {
-                self.view.frame.origin.y -= self.scrollAmount
-            } else {
-                self.view.frame.origin.y += self.scrollAmount
-            }
-    
-            UIView.commitAnimations()
-    
-        }
-    
-    
-    
-        //MARK: - UITextField Delegate Methods
-    
-        func textFieldDidBeginEditing(textField: UITextField!) {
-            activeTextField = textField
-        }
-    
-        func textFieldDidEndEditing(textField: UITextField!) {
-            activeTextField = nil
-        }
-    
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
+    func scrollView(move:Bool) {
+        //println("Before:\(scrollAmount)")
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(0.2)
+        
+        if move {
+            self.view.frame.origin.y -= self.scrollAmount
+        } else {
+            self.view.frame.origin.y = self.initialFrame.origin.y
         }
         
-        
-        override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-            if (activeTextField != nil) {
-                activeTextField.resignFirstResponder();
-            }
+        UIView.commitAnimations()
+    }
+    
+    
+    //MARK: - UITextField Delegate Methods
+    func textFieldDidBeginEditing(textField: UITextField!) {
+        activeTextField = textField
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField!) {
+        activeTextField = nil
+    }
+    
+
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        if (activeTextField != nil) {
+            activeTextField.resignFirstResponder();
         }
-
-
+    }
+    
+    
 }
